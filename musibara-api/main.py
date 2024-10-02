2102aa8b69967ebfb1972425cc9bca7e7c14580f
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-10-02 17:53:02 -0500

    Cors policy update

--- musibara-api/main.py
+++ musibara-api/main.py
@@ -1,7 +1,23 @@
 from typing import Annotated
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 
 app = FastAPI()
 app.include_router(router)
+
+
+'''
+Enable CORSMiddleware
+
+Note: This will need to be changed in the future
+        once we get AWS up and going.
+'''
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
