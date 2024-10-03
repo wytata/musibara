6393a11cd9d7a5ba461c017ab15756a39474660a
Wyatt Cade <cadewya@tamu.edu>
2024-10-03 10:33:30 -0500

    Accept username and password through /api/users/token route on POST

--- musibara-api/routes/users.py
+++ musibara-api/routes/users.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
-from services.users import getAllUsers
+from fastapi import APIRouter, Depends 
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from services.users import getAllUsers, userLogin
 
 userRouter = APIRouter()
 
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 @userRouter.get("/")
-async def response():
+async def getAllUsersResponse():
     return await getAllUsers()
+
+@userRouter.post("/token")
+async def userLoginResponse(formData: OAuth2PasswordRequestForm = Depends()):
+    return await userLogin(formData)
--- musibara-api/services/users.py
+++ musibara-api/services/users.py
@@ -1,5 +1,17 @@
 import json
+from typing_extensions import deprecated
 from config.db import db
+
+from fastapi import APIRouter, Depends 
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+
+SECRET_KEY="9c3126ab71aab65b1a254c314f57a3af42dfbe896e21b2c12bee8f60c027cf6"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRATION_MINUTES=30
+
+passwordContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 async def getAllUsers():
     cursor = db.cursor()
@@ -10,3 +22,24 @@ async def getAllUsers():
     result = [dict(zip(columnNames, row)) for row in rows]
 
     return result
+
+def authenticateUser(username: str, password: str):
+    cursor = db.cursor()
+    cursor.execute(f'SELECT name, password FROM users WHERE name = \'{username}\'')
+    result = cursor.fetchone()
+    print(result)
+
+
+async def userLogin(formData: OAuth2PasswordRequestForm = Depends()):
+    print(formData)
+    username, password = formData.username, formData.password
+    authenticateUser(username, password)
+
+
+
+
+
+
+
+
+
