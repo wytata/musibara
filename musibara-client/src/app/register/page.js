c9431eb4014c9a273427e505bab6643e281b04bf
Wyatt Cade <cadewya@tamu.edu>
2024-10-08 21:45:46 -0500

    Add very basic login/register pages to test frontend-backend connection of auth

---
+++ musibara-client/src/app/register/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import RegistrationForm from "@/components/RegistrationForm";
+
+export default function () {
+  return (
+       <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <RegistrationForm/>
+      </div>
+    </main>
+  );
+}
---
+++ musibara-client/src/components/RegistrationForm.js
@@ -0,0 +1,78 @@
+export default function RegistrationForm() {
+  return (
+    <form className="space-y-3">
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+        <h1 className={`mb-3 text-2xl text-black`}>
+          Welcome to Musibara!
+        </h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-black"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="username"
+                type="username"
+                name="username"
+                placeholder="Enter your new username"
+                required
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-black-500"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter your new password"
+                required
+                minLength={6}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <label
+            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+            htmlFor="password"
+          >
+            Confirm Password
+          </label>
+          <div className="relative">
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-black-500"
+              id="passwordConfirmation"
+              type="passwordConfirmation"
+              name="passwordConfirmation"
+              placeholder="Confirm your new password"
+              required
+              minLength={6}
+            />
+          </div>
+        </div>
+        </div>
+        <button>
+        </button>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+        </div>
+    </form>
+  );
+
+}
