7017c7fefd15d815e4f71a92f6451841f51874db
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-10-09 17:58:07 -0500

    added read me file

--- musibara-api/routes/__init__.py
+++ musibara-api/routes/__init__.py
@@ -5,5 +5,5 @@ from .users import userRouter
 from .posts import postsRouter
 
 router = APIRouter()
-router.include_router(userRouter, prefix="/api/users")
+#router.include_router(userRouter, prefix="/api/users")
 router.include_router(postsRouter, prefix="/api/posts")
