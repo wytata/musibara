7966abd9ccff018f8e35ebea0239d950d2b067ef
Wyatt <cadewya@tamu.edu>
2024-10-20 17:08:53 -0500

    Pre-format comment data for ease in front-end. Note, we absolutely need to test this further, but to avoid hindering progress for now, this is being pushed on just this branch.

--- musibara-api/services/postComments.py
+++ musibara-api/services/postComments.py
@@ -13,5 +13,30 @@ async def getCommentsByPostId(postId: int):
     columnNames = [desc[0] for desc in cursor.description]
     result = [dict(zip(columnNames, row)) for row in rows]
 
-    return result
+    formattedResult = {}
+    parentCache = {}
+    for comment in result:
+        comment_id = comment['postcommentid']
+        del comment['postcommentid']
+        comment['children'] = {}
+        if comment['parentpostcomment'] is None:
+            formattedResult[comment_id] = comment
+        else:
+            parentCache[comment_id] = comment['parentpostcomment']
+            parentsList = []
+            curr_id = comment_id
+            while curr_id in parentCache:
+                parentsList.append(parentCache[curr_id])
+                curr_id = parentCache[curr_id]
 
+            targetDict = formattedResult
+            for id in reversed(parentsList):
+                targetDict = targetDict[id]['children']
+
+            targetDict[comment_id] = comment
+
+            print(parentsList)
+
+    print(formattedResult)
+    print(parentCache)
+    return formattedResult
