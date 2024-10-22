3101f29b1180c39954c618302e51a3771b1ba2b0
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-10-22 15:08:00 -0500

    Merge server changes

--- musibara-api/config/db.py
+++ musibara-api/config/db.py
@@ -1,9 +1,6 @@
 import psycopg2
 import dotenv
 import os
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 dotenv.load_dotenv()
 
@@ -11,10 +8,6 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
-print("DB_USER:", DB_USER)
-print("DB_PASS:", DB_PASS)
-print("DB_HOST:", DB_HOST)
-print("DB_NAME:", DB_NAME)
 
 db = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port = '5432')
 
