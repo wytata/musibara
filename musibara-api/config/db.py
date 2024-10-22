8e9c2572b8e33bf0f9139c53dd69439d84d5299e
EC2 Default User <ec2-user@ip-172-31-22-79.ec2.internal>
2024-10-22 18:48:33 +0000

    Init server changes

--- musibara-api/config/db.py
+++ musibara-api/config/db.py
@@ -1,6 +1,9 @@
 import psycopg2
 import dotenv
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 dotenv.load_dotenv()
 
@@ -8,5 +11,10 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
+print("DB_USER:", DB_USER)
+print("DB_PASS:", DB_PASS)
+print("DB_HOST:", DB_HOST)
+print("DB_NAME:", DB_NAME)
 
-db = psycopg2.connect(f'dbname={DB_NAME} user={DB_USER} password={DB_PASS} host={DB_HOST}')
+db = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port = '5432')
+
