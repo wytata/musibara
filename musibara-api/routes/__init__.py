a4485e4f59dd5710d99717091dfd5801952bd77f
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-10-27 21:53:53 -0500

    Route for homebar

--- musibara-api/routes/__init__.py
+++ musibara-api/routes/__init__.py
@@ -1,12 +1,15 @@
-__all__ = ["users"]
+__all__ = ["router"]
 
+from sys import prefix
 from fastapi import APIRouter
 from .users import userRouter
 from .posts import postsRouter
 from .postActions import postsActionsRouter
+from .homebar import homebar_router
 
 router = APIRouter()
 router.include_router(userRouter, prefix="/api/users")
 router.include_router(postsRouter, prefix="/api/posts")
 router.include_router(postsActionsRouter, prefix="/api/postsActions")
+router.include_router(homebar_router, prefix="/api")
 
