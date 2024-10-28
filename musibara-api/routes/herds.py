e6aa0528c696e3b93d5c3b2252986a599fb580bd
Wyatt Cade <cadewya@tamu.edu>
2024-10-28 12:15:12 -0500

    Set up route for creating herds, leaving s3 bucket image storage for later when the bucket is created

--- musibara-api/routes/herds.py
+++ musibara-api/routes/herds.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Depends, Response, Request
 import fastapi
-from fastapi import Form
+from fastapi import Form, UploadFile, File
 from typing_extensions import Annotated
-from services.herds import getHerdById
+from services.herds import createHerd, getHerdById
+from musibaraTypes.herds import Herd
 
 herdsRouter = APIRouter()
 
 @herdsRouter.get("/{herd_id}")
 async def getHerdResponse(herd_id: int):
     return await getHerdById(herd_id)
+
+@herdsRouter.put("/new")
+async def newHerdResponse(
+        image: UploadFile = File(None),
+        name: str = Form(...),
+        description: str = Form(...)
+    ):
+    return await createHerd(image, name, description)
