1f0e5e9ffa6b3461532f5476de5c637495364d2f
willheeney03 <99232572+willheeney03@users.noreply.github.com>
2024-10-29 16:22:10 -0500

    complete merge

--- musibara-client/src/components/Comment.js
+++ musibara-client/src/components/Comment.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
+import { IconButton } from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Comment = ({ comment, level = 0 }) => {
     const [showMore, setShowMore] = useState(false);
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleLikeClick = () => {
+        //TODO: make api calls here to update comments likes. May also need some kind of user information if we are storing who liked what ?
+        if(isLiked) {
+            console.log("unlike comment");
+            comment.likescount -= 1; //only here to show how it will look
+        }
+        else {
+            console.log("like comment");
+            comment.likescount += 1; //only here to show how it will look
+        }
+
+        setIsLiked(!isLiked);
+    }
 
     const toggleShowMore = () => {
         setShowMore(!showMore);
@@ -13,8 +31,13 @@ const Comment = ({ comment, level = 0 }) => {
         }}>
             <div style={{ fontWeight: 'bold' }}>{comment.username}</div>
             <div>{comment.content}</div>
-            <div>Likes: {comment.likes}</div>
-            <div style={{ fontSize: 'small', color: 'gray' }}>{new Date(comment.createdAt).toLocaleString()}</div>
+            <div>
+                <IconButton onClick={handleLikeClick}>
+                    {isLiked ? (<FavoriteIcon />) : (<FavoriteBorderIcon />)}
+                </IconButton>
+                {comment.likescount}
+            </div>
+            <div style={{ fontSize: 'small', color: 'gray' }}>{new Date(comment.createdts).toLocaleString()}</div>
 
             {/* Only show replies up to level 1, collapse beyond that */}
             {comment.replies.length > 0 && (
--- musibara-client/src/components/PostModal.js
+++ musibara-client/src/components/PostModal.js
@@ -8,108 +8,26 @@ import Comment from "./Comment";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const PostModal = ({ open, handleClose, postid }) => {
-    // const [post, setPost] = useState(null);
-    // const [postComments, setPostComments] = useState(null);
+    const [post, setPost] = useState(null);
+    const [postComments, setPostComments] = useState(null);
 
-    // const fetchPost = async () => {
-    //     const postResponse = await fetch(apiUrl + `/api/posts/${postid}`)
-    //     const jsonData = await postResponse.json()
-    //     setPost(jsonData);
-    // }
-
-    // const fetchPostComments = async () => {
-    //     const commentsResponse = await fetch(apiUrl + `/api/postcomments/${postid}`)
-    //     const jsonData = await commentsResponse.json()
-    //     setPostComments(jsonData);
-    // }
-
-    // const post1 = [
-    //     {
-    //         postid: 1,
-    //         userid: "djCoolBeats",
-    //         title: "Check out my new track: Chill Vibes",
-    //         content: "I just dropped a new chill track, perfect for those late-night coding sessions! Give it a listen and let me know what you think.",
-    //         likescount: 120,
-    //         numcomments: 2,
-    //         comments: [
-    //             { username: 'musicFan', text: 'Love this track!' },
-    //             { username: 'lofiLover', text: 'Perfect for studying!' }
-    //         ],
-    //         tags: ["chill", "lofi", "electronic"],
-    //     }
-    // ]
-
-    const post = {
-        postid: 1,
-        username: "djCoolBeats",
-        title: "Check out my new track: Chill Vibes",
-        content: "I just dropped a new chill track, perfect for those late-night coding sessions! Give it a listen and let me know what you think.",
-        likescount: 120,
-        numcomments: 6,
-        tags: ["chill", "lofi", "electronic"],
+    const fetchPost = async () => {
+        const postResponse = await fetch(apiUrl + `/api/posts/${postid}`)
+        const jsonData = await postResponse.json()
+        jsonData.tags = ["Hip Hop", "Electronic", "Rock"]; //hardcoded because api route does not return tags since tags table not set up yet
+        setPost(jsonData);
     }
 
-    const postComments = {
-        postid: 1,
-        comments: [
-            {
-                commentId: 1,
-                username: "user1",
-                content: "This is the first comment on the post.",
-                likes: 20,
-                createdAt: "2024-10-22T14:45:00Z",
-                replies: [
-                    {
-                        commentId: 2,
-                        username: "user2",
-                        content: "This is a reply to the first comment.",
-                        likes: 5,
-                        createdAt: "2024-10-22T15:00:00Z",
-                        replies: [
-                            {
-                                commentId: 4,
-                                username: "user5",
-                                content: "This is a reply to the second comment.",
-                                likes: 7,
-                                createdAt: "2024-10-22T15:00:00Z",
-                                replies: []
-                            },
-                            {
-                                commentId: 5,
-                                username: "user6",
-                                content: "This is another reply to the second comment.",
-                                likes: 8,
-                                createdAt: "2024-10-22T15:00:00Z",
-                                replies: []
-                            }
-                        ]
-                    },
-                    {
-                        commentId: 10,
-                        username: "user9",
-                        content: "This is the second reply to the first comment.",
-                        likes: 11,
-                        createdAt: "2024-10-22T15:05:00Z",
-                        replies: []
-                    }
-                ]
-            },
-            {
-                commentId: 3,
-                username: "user3",
-                content: "This is the second comment on the post.",
-                likes: 10,
-                createdAt: "2024-10-22T15:05:00Z",
-                replies: []
-            }
-        ],
+    const fetchPostComments = async () => {
+        const commentsResponse = await fetch(apiUrl + `/api/postcomments/${postid}`)
+        const jsonData = await commentsResponse.json()
+        setPostComments(jsonData);
     }
 
-    //fetch data...
-    // useEffect(() => {
-    //     fetchPost();
-    //     fetchPostComments();
-    // }, [postid]);
+    useEffect(() => {
+        fetchPost();
+        fetchPostComments();
+    }, [postid]);
 
     return (
         <Dialog
@@ -157,7 +75,7 @@ const PostModal = ({ open, handleClose, postid }) => {
                             </Typography>
                         </Box>
 
-                        {post.numcomments === 0 ? (
+                        {post.numcomments === 0 || !postComments ? (
                             <div>No comments yet</div>
                         ) : (
                             postComments.comments.map(comment => (
