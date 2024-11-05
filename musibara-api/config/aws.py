740e18011aa7f4ac08c787a418bf2a57b86fccf1
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-05 17:04:44 -0600

    connect to aws s3 bucket

---
+++ musibara-api/config/aws.py
@@ -0,0 +1,22 @@
+import boto3
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+KEY = os.getenv("AWS_KEY")
+SEC_KEY = os.getenv("AWS_SEC_KEY")
+REGION = os.getenv("AWS_REGION")
+BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+
+
+def create_s3_client():
+    return boto3.client(
+        's3',
+        aws_access_key_id=KEY,
+        aws_secret_access_key=SEC_KEY,
+        region_name=REGION
+    )
+def get_bucket_name():
+    return BUCKET_NAME
+
