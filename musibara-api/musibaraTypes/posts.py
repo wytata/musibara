4e79e9ba734da082862d7694bcb9507ce5245da4
Will Heeney <willheeney03@tamu.edu>
2024-11-06 20:40:31 -0600

    add basic create post functionality

--- musibara-api/musibaraTypes/posts.py
+++ musibara-api/musibaraTypes/posts.py
@@ -7,7 +7,21 @@ NOTE: This route is prefixed with posts/
 postsRouter = APIRouter()
 
 class MusibaraPostType(TypedDict):
-    userid: int
+    username: str
+    title: str
     content: str
-    likes: int
+    herdname: str
+    #tags: str - add this once the functionality is there on frontend
+    #image: str - maybe add this later ?
+
+    
+    # postid: int
+    # userid: int
+    # content: str
+    # likescount: int
+    # commentcount: int
+    # imageid: int
+    # herdid: int
+    # createdts: str
+    # title: str
 
