f74918d0935aaa9b02ed29bf5e6a2aa152b9dac4
Will Heeney <willheeney03@tamu.edu>
2024-11-06 20:43:45 -0600

    remove comment

--- musibara-api/musibaraTypes/posts.py
+++ musibara-api/musibaraTypes/posts.py
@@ -14,14 +14,4 @@ class MusibaraPostType(TypedDict):
     #tags: str - add this once the functionality is there on frontend
     #image: str - maybe add this later ?
 
-    
-    # postid: int
-    # userid: int
-    # content: str
-    # likescount: int
-    # commentcount: int
-    # imageid: int
-    # herdid: int
-    # createdts: str
-    # title: str
 
