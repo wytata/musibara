356da649712f96bf9e6755e625f9fd52e615ebce
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-13 00:15:02 -0600

    Removed redundant function, Added refresh cookie, and check for expired cookie

--- musibara-api/services/user_auth.py
+++ musibara-api/services/user_auth.py
@@ -12,57 +12,49 @@ ALGORITHM="HS256"
 ACCESS_TOKEN_EXPIRATION_MINUTES=30
 
 
-def get_username_from_cookie(request: Request) -> str | None:
+# NOTE: This also checks if the cookie is expired    
+def get_id_username_from_cookie(request: Request) :
     cookies = request.cookies
-    access_token = cookies.get("accessToken")
-    if not access_token:
-       return None
-    username: str = ""
     try:
+        access_token = cookies.get("accessToken")
+        if not access_token:
+            return None, None
+        id: str = ""
+        username: str = ""
+        time: str = ""
+
         payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-
-        if not username:
-            print('No username in tokenfor get_current_user()')
-            return None
-    except jwt.InvalidTokenError:
-        print('InvalidTokenError in get_current_user()')
-        return None
-
-    return username
-
-
-def get_username_id_from_cookie(request: Request) :
-    cookies = request.cookies
-    access_token = cookies.get("accessToken")
-    if not access_token:
-       return None
-    id: str = ""
-    username: str = ""
-    try:
-        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
-        id = payload.get("id")
-        if not id:
-            print('No id in tokenfor get_current_user()')
-
-        username = payload.get("sub")
-        if not username:
-            print('No username in tokenfor get_current_user()')
-
-    except jwt.InvalidTokenError:
-        print('InvalidTokenError in get_current_user()')
-        return None, None
         
-    return id, username
+        # NOTE: Not sure if we should throw errors, or return None and let caller handle 
+        id = payload.get("id")
+        username = payload.get("sub")
+        time = payload.get("exp")
+        
+        # Invalidate expired token
+        if is_time_expired(request):
+            id = None
+            username = None
 
+        return id, username
 
-def get_cookie(response: Response, username:str, id: int = None):
+    except jwt.ExpiredSignatureError:
+        print("Access Token Expired")
+        return None, None
+    except jwt.InvalidTokenError:
+        print("Invalid Access Token")
+        return None, None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None, None
+
+def get_cookie(response: Response, username:str, id= None):
     if id is None:
        result = get_user_id(username)
        id = result
 
     accessTokenExpiration = timedelta(minutes=ACCESS_TOKEN_EXPIRATION_MINUTES)
     dataToEncode = {"sub": username, "exp": datetime.now(timezone.utc)+accessTokenExpiration, "id": id}
+    print(dataToEncode)
     accessToken = jwt.encode(dataToEncode, SECRET_KEY, algorithm=ALGORITHM)
     response.set_cookie(
         key="accessToken",
@@ -75,7 +67,49 @@ def get_cookie(response: Response, username:str, id: int = None):
     )
 
 
-# Had to duplicate this from users due to circular import
+def refresh_cookie(request: Request):
+    user_id, username = get_id_username_from_cookie(request)
+    if (not user_id or not username) and is_time_expired(request):
+        raise HTTPException(status_code=403, detail="Invalid refresh token")
+    
+    get_cookie(request, username, user_id)
+    return True
+
+
+def is_time_expired(request: Request):
+    cookies = request.cookies
+    try:
+        access_token = cookies.get("accessToken")
+        if not access_token:
+            return False
+        time_str: str = ""
+
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        
+        # NOTE: Not sure if we should throw errors, or return None and let caller handle
+        time_str:str = payload.get("exp")
+        time = datetime.fromisoformat(time_str)
+        if time.tzinfo is None:
+            time = time.replace(tzinfo=timezone.utc)
+        current_time = datetime.now(timezone.utc)
+        if current_time>time:
+            print("Token is expired")
+            return False
+
+        return True
+
+    except jwt.ExpiredSignatureError:
+        print("Access Token Expired")
+        return False
+    except jwt.InvalidTokenError:
+        print("Invalid Access Token")
+        return False
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+
+# NOTE: Had to duplicate this from users due to circular import
 def get_user_id(username:str):
     db = get_db_connection()
     cursor = db.cursor()
