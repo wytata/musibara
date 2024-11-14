30c304999feab9344d903abe5f65161d04cf0983
Wyatt <cadewya@tamu.edu>
2024-11-13 22:36:37 -0600

    Add NEXT_PUBLIC_SPOTIFY_REDIRECT_URI as env variable in spotifyClient.js

--- musibara-client/src/utilities/spotifyClient.js
+++ musibara-client/src/utilities/spotifyClient.js
@@ -3,7 +3,7 @@ import SpotifyWebApi from "spotify-web-api-node";
 const spotifyClient = new SpotifyWebApi({
   clientId: process.env.NEXT_PUBLIC_SPOTIFY_ID,
   clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_SECRET,
-  redirectUri: "http://localhost:3000/profile"
+  redirectUri: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI
 })
 
 export default spotifyClient
