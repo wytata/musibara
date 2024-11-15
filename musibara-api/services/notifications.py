62ffa41ec63f661322c9a207d5893b6a90aa3487
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-15 15:18:55 -0600

    Fixed query

--- musibara-api/services/notifications.py
+++ musibara-api/services/notifications.py
@@ -4,8 +4,40 @@ from fastapi import Request, HTTPException, status
 from config.db import get_db_connection
 from .s3bucket_images import get_image_url
 
+
+async def get_and_format_url(columns, rows, type):
+    # Format image url
+        columnNames = []
+        image_index = -1
+        bucket_index = -1
+        key_index = -1
+        for i, desc in enumerate(columns):
+            if desc[0]=="profilephoto":
+                columnNames.append("url")
+                image_index = i
+            elif desc[0]=="bucket":
+                bucket_index = i
+            elif desc[0]=="key":
+                key_index=i
+            else:
+                columnNames.append(desc[0])
+        columnNames.append("notificationtype")
+
+        result = []
+        for row in rows:
+            #Getting temp image urls
+            url = await get_image_url(row[image_index], row[bucket_index], row[key_index])
+            row = list(row)
+            row[image_index] = url
+            result = dict(zip(columnNames, row))
+            result["notificationtype"]= type
+
+        return result
+
 async def get_users_notifications(request:Request):
-    user_id, username = get_id_username_from_cookie(request)
+    #user_id, username = get_id_username_from_cookie(request)
+    user_id = 2
+    username = "rdrake"
 
     if not user_id or not username:
         raise HTTPException(
@@ -29,7 +61,7 @@ async def get_users_notifications(request:Request):
             p.userid= %s
         ORDER BY
             pl.createdts
-        DESC LIMIT 100;
+        DESC LIMIT 3;
         """
     
     query_comments = """
@@ -47,23 +79,12 @@ async def get_users_notifications(request:Request):
             p.userid= %s
         ORDER BY 
             pc.createdts
-        DESC LIMIT 100;
+        DESC LIMIT 3;
         """
 
-    """
-    NOTE: for reference
-    
-    CREATE TABLE IF NOT EXISTS postcommentlikes(
-        postcommentid INTEGER,
-        userid INTEGER,
-        createdts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        FOREIGN KEY (postcommentid) REFERENCES postcomments(postcommentid),
-        FOREIGN KEY (userid) REFERENCES users(userid)
-    );
-    """
     query_comment_likes = """
         SELECT 
-            pcl.userid, u.username, pc.content, pc.postid, pcl.createdts, u.profilephoto, i.bucket, i.key 
+            pcl.userid, u.username, pc.postcommentid, pc.content, pc.postid, pcl.createdts, u.profilephoto, i.bucket, i.key 
         FROM 
             postcommentlikes pcl
         JOIN
@@ -79,8 +100,7 @@ async def get_users_notifications(request:Request):
         DESC LIMIT 100;
         """
     
-    # user who commented, your parent comment, image of user who commented
-    query_comment_replys = """
+    query_comment_replies = """
         SELECT 
             pc.parentcommentid, pc.userid, u.username, pc.postcommentid, pc.content, pc.postid, pc.createdts, u.profilephoto, i.bucket, i.key 
         FROM 
@@ -93,15 +113,15 @@ async def get_users_notifications(request:Request):
         (
             SELECT postcommentid 
             FROM postcomments 
-            WHERE userid =2
+            WHERE userid =%s
         )
         GROUP BY
             pc.parentcommentid, pc.userid, u.username, pc.postcommentid, pc.content, pc.postid, pc.createdts, u.profilephoto, i.bucket, i.key
         ORDER BY 
             pc.createdts
-        DESC LIMIT 100;
+        DESC LIMIT 3;
         """
-    params = [username]
+    params = [user_id]
     
     try:  
         
@@ -114,62 +134,31 @@ async def get_users_notifications(request:Request):
         cursor.execute(query_comments, params)
         rows_comments = cursor.fetchall()
         columns_comments = cursor.description
+        cursor.execute(query_comment_likes, params)
+        rows_comment_likes = cursor.fetchall()
+        columns_comment_likes = cursor.description
+        cursor.execute(query_comment_replies, params)
+        rows_comment_replies = cursor.fetchall()
+        columns_comment_replies = cursor.description
         cursor.close()
         
-        # Format likes with image url
-        columnNames = []
-        image_index = -1
-        bucket_index = -1
-        key_index = -1
-        for i, desc in enumerate(columns_likes):
-            if desc[0]=="profilephoto":
-                columnNames.append("url")
-                image_index = i
-            elif desc[0]=="bucket":
-                bucket_index = i
-            elif desc[0]=="key":
-                key_index=i
-            else:
-                columnNames.append(desc[0])
-
-        result_likes = []
-        for row in rows_likes:
-            #Getting temp image urls
-            url = await get_image_url(row[image_index], row[bucket_index], row[key_index])
-            row = list(row)
-            row[image_index] = url
-            result_likes = dict(zip(columnNames, row))
-            
-            
-        # Format comments with image url
-        columnNames = []
-        image_index = -1
-        bucket_index = -1
-        key_index = -1
-        for i, desc in enumerate(columns_comments):
-            if desc[0]=="profilephoto":
-                columnNames.append("url")
-                image_index = i
-            elif desc[0]=="bucket":
-                bucket_index = i
-            elif desc[0]=="key":
-                key_index=i
-            else:
-                columnNames.append(desc[0])
-
-        result_comments = []
-        for row in rows_comments:
-            #Getting temp image urls
-            url = await get_image_url(row[image_index], row[bucket_index], row[key_index])
-            row = list(row)
-            row[image_index] = url
-            result_comments = dict(zip(columnNames, row))
-    
+        likes_result = await get_and_format_url(columns_likes, rows_likes, "likes")  
+        comments_result = await get_and_format_url(columns_comments, rows_comments, "comments")
+        comment_likes_result = await get_and_format_url(columns_comment_likes, rows_comment_likes, "commentlikes")
+        print(comment_likes_result)
+        comment_replies_result = await get_and_format_url(columns_comment_replies, rows_comment_replies, "commentreplies")
+        
+        result = [likes_result, comments_result, comment_likes_result, comment_replies_result]
+        result.sort()
+        
+        return result
+        
+        
     except Exception as e:
         print(f"ERROR: {e}")
         raise HTTPException(
-            status_code=status.HTTP_500,
-            detail="Incorrect username or password",
+            status_code=500,
+            detail="Internal server error with notifications",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
