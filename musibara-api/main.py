4641ef8026b5fc10848c7bd6dcb8ffdb67eb0282
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-15 17:15:09 -0600

    My fault

--- musibara-api/main.py
+++ musibara-api/main.py
@@ -1,10 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 from services.s3bucket_images import run_threaded_garbage_collector
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import Response
 import threading
 import musicbrainzngs
 import dotenv
@@ -35,42 +33,11 @@ Note: This will need to be changed in the future
 
 ORIGIN = os.getenv("ORIGIN").split(',')
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[ORIGIN],
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
-#     allow_headers=["Access-Control-Allow-Headers", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Set-Cookie", "Access-Control-Allow-Credentials"],
-#     #allow_headers = ["*"]
-# )
-ALLOWED_IPS = ['172.31.17.116', '127.0.0.1']
-
-class CustomCORSMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        # Get the origin header
-        origin = request.headers.get('Origin')
-
-        # Get the IP address from the request
-        ip_address = request.client.host
-
-        # Check if the IP is allowed
-        if ip_address in ALLOWED_IPS:
-            # If allowed IP, allow the request to proceed
-            response = await call_next(request)
-            
-            # Set the CORS headers if 'Origin' is present
-            if origin:
-                response.headers["Access-Control-Allow-Origin"] = origin
-                response.headers["Access-Control-Allow-Credentials"] = "true"
-                response.headers["Access-Control-Allow-Methods"] = "GET, POST, HEAD, OPTIONS"
-                response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, Set-Cookie, Access-Control-Allow-Origin"
-            
-            return response
-        else:
-            # If the IP is not allowed, deny the request
-            return Response("Forbidden", status_code=403)
-
-
-# Add the custom CORS middleware
-app.add_middleware(CustomCORSMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ORIGIN],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
+    allow_headers=["Access-Control-Allow-Headers", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Set-Cookie", "Access-Control-Allow-Credentials"],
+)
 
