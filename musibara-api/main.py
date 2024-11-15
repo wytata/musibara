b4a4aa21c680fa91135b340e2fe7350a1c202817
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-15 00:31:01 -0600

    Deleted comment

--- musibara-api/main.py
+++ musibara-api/main.py
@@ -39,6 +39,5 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
     allow_headers=["Access-Control-Allow-Headers", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Set-Cookie", "Access-Control-Allow-Credentials"],
-    #allow_headers = ["*"]
 )
 
