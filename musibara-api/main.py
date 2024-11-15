0725b0c5f332d41b3b3c53ae45ff877fb094280b
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-15 17:03:42 -0600

    revive main

--- musibara-api/main.py
+++ musibara-api/main.py
@@ -1,10 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 from services.s3bucket_images import run_threaded_garbage_collector
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import Response
 import threading
 import musicbrainzngs
 import dotenv
@@ -35,41 +33,11 @@ Note: This will need to be changed in the future
 
 ORIGIN = os.getenv("ORIGIN").split(',')
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[ORIGIN],
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
-#     allow_headers=["Access-Control-Allow-Headers", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Set-Cookie", "Access-Control-Allow-Credentials"],
-#     #allow_headers = ["*"]
-# )
-ALLOWED_IPS = ['165.91.0.132', '127.0.0.1', 'musibara.com']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ORIGIN],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
+    allow_headers=["Access-Control-Allow-Headers", "Content-Type", "Authorization", "Access-Control-Allow-Origin", "Set-Cookie", "Access-Control-Allow-Credentials"],
+)
 
-class CustomCORSMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        origin = request.headers.get('Origin')
-        ip_address = request.headers.get('X-Forwarded-For', request.client.host)
-
-        if ip_address in ALLOWED_IPS or origin in ALLOWED_IPS:
-            response = await call_next(request)
-
-            if request.method == "OPTIONS":
-                response = Response(status_code=200)
-                response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-                response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, Set-Cookie, Access-Control-Allow-Origin"
-                response.headers["Access-Control-Allow-Origin"] = origin
-                response.headers["Access-Control-Allow-Credentials"] = "true"
-
-            else:
-                if origin:
-                    response.headers["Access-Control-Allow-Origin"] = origin
-                    response.headers["Access-Control-Allow-Credentials"] = "true"
-                    response.headers["Access-Control-Allow-Methods"] = "GET, POST, HEAD, OPTIONS"
-                    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, Set-Cookie, Access-Control-Allow-Origin"
-
-            return response
-
-        else:
-            return Response("Forbidden", status_code=403)
-
-app.add_middleware(CustomCORSMiddleware)
