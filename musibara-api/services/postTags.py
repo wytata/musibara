baa5c8ca57ec7c6bebaa1fd6fec96d089c23cca7
Wyatt <cadewya@tamu.edu>
2024-11-18 20:36:48 -0600

    Complete set_post_tags function given post with defined tags

--- musibara-api/services/postTags.py
+++ musibara-api/services/postTags.py
@@ -2,10 +2,18 @@ import json
 from typing import Union, List, Dict, Optional
 from config.db import get_db_connection
 
-async def set_post_tag(tags: list[dict], post_id: int):
+async def set_post_tags(tags: list[dict], post_id: int):
     # Function to be called only after successful insertion of post into database
+    # Note: treat return value False as error
     if not all(tag['type'].lower() in ["song", "artist", "album"] for tag in tags):
+        return False
+    try:
+        db = get_db_connection()
+        cursor = db.cursor()
+        values_list = ','.join(cursor.mogrify(f"({str(post_id)}, %s, %s, %s)", tuple(tag.values())).decode('utf-8') for tag in tags)
+        cursor.execute("INSERT INTO posttags (postid, resourcetype, mbid, name) VALUES " + values_list)
+        db.commit()
+        return True
+    except Exception as e:
+        print(e)
         return None
-    for tag in tags:
-        print(tag)
-
