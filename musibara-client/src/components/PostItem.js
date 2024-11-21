f98821938840aa359a340a5769d541ab2fae2e94
willheeney03 <99232572+willheeney03@users.noreply.github.com>
2024-11-21 16:29:42 -0600

    Make PostItems and PostModals display tags now. Significantly reduce API calls made since PostModal only fetches comments when its open and does not fetch post again

--- musibara-client/src/components/PostItem.js
+++ musibara-client/src/components/PostItem.js
@@ -5,6 +5,9 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import PostModal from './PostModal';
+import PersonIcon from '@mui/icons-material/Person';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import AlbumIcon from '@mui/icons-material/Album';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -15,14 +18,14 @@ const PostItem = ({ post }) => {
     const [likesCount, setLikesCount] = useState(post.likescount);
 
     useEffect(() => {
-        
+
         const fetchIsLiked = async () => {
             const likedStatus = await getIsLiked(post.postid);
             setIsLiked(likedStatus);
         };
-    
+
         fetchIsLiked(post.postid);
-    
+
     }, []);
 
     const getIsLiked = async (postid) => {
@@ -33,7 +36,7 @@ const PostItem = ({ post }) => {
         console.log(isLikedJson.isLiked);
         return isLikedJson.isLiked;
     }
-    
+
     const handlePostLikeClick = async () => {
         const endpoint = isLiked ? '/api/content/posts/unlike' : '/api/content/posts/like';
         const response = await fetch(apiUrl + endpoint, {
@@ -47,7 +50,7 @@ const PostItem = ({ post }) => {
             })
         })
 
-        if(response.ok) {
+        if (response.ok) {
             const newLikes = isLiked ? likesCount - 1 : likesCount + 1;
             setLikesCount(newLikes);
             setIsLiked(!isLiked);
@@ -62,45 +65,62 @@ const PostItem = ({ post }) => {
         setOpenModal(false);
     };
 
-    post.tags = ["Hip Hop", "Electronic", "Rock"]; // static data until tag db table is set up and route is updated to return post tags
-
     return (
         <React.Fragment>
             <Card variant="outlined" sx={{ display: 'flex', mb: 2, cursor: 'pointer', borderRadius: '2rem', fontFamily: 'Cabin', color: '#264653' }} onClick={handleOpenModal}>
                 {/* Likes Section */}
-                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', p: 2, minWidth: 80,  background: '#e6eded', color: '#264653' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', p: 2, minWidth: 80, background: '#e6eded', color: '#264653' }}>
                     <IconButton onClick={(event) => {
-                    event.stopPropagation();  // Prevents modal from opening
-                    handlePostLikeClick();
-                }} >
+                        event.stopPropagation();  // Prevents modal from opening
+                        handlePostLikeClick();
+                    }} >
                         {isLiked ? (<FavoriteIcon />) : (<FavoriteBorderIcon />)}
                     </IconButton>
-                    <Typography variant="h6" sx={{ my: 1, fontFamily:'Cabin'}}>
+                    <Typography variant="h6" sx={{ my: 1, fontFamily: 'Cabin' }}>
                         {likesCount}
                     </Typography>
                 </Box>
 
                 {/* Post Content */}
                 <CardContent sx={{ flexGrow: 1, background: '#e6eded' }}>
-                    <Typography variant="h6" style={{fontFamily: 'Cabin'}}>{post.title}</Typography>
+                    <Typography variant="h6" style={{ fontFamily: 'Cabin' }}>{post.title}</Typography>
                     <Typography variant="subtitle2" color="textSecondary" sx={{ mb: 1, fontFamily: 'Cabin' }}>
                         posted by @{post.username}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" sx={{ mb: 1.5 , fontFamily: 'Cabin'}}>
+                    <Typography variant="body2" color="textSecondary" sx={{ mb: 1.5, fontFamily: 'Cabin' }}>
                         {post.content}
                     </Typography>
 
-                    {/* Tags */}
-                    <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap', mb: 1.5, fontFamily:'Cabin'}}>
-                        {post.tags.map((tag, index) => (
-                            <Chip key={index} label={`#${tag}`} size="small" color="primary" sx={{fontFamily: 'Cabin', background: "#617882"}}/>
-                        ))}
+                    {/* Display Tags to be added to post here */}
+                    <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap', mb: 1.5, fontFamily: 'Cabin' }}>
+                        {post.tags.map((tag, index) => {
+                            let icon;
+
+                            if (tag.tag_type === 'songs') {
+                                icon = <MusicNoteIcon />;
+                            } else if (tag.tag_type === 'artists') {
+                                icon = <PersonIcon />;
+                            } else if (tag.tag_type === 'albums') {
+                                icon = <AlbumIcon />;
+                            }
+
+                            return (
+                                <Chip
+                                    key={index}
+                                    label={`${tag.name || tag.title}`}
+                                    size="small"
+                                    color="primary"
+                                    style={{ background: "#617882", color: "#fff" }}
+                                    icon={icon}
+                                />
+                            );
+                        })}
                     </Box>
 
                     {/* Comments */}
                     <Box sx={{ display: 'flex', alignItems: 'center' }}>
                         <ChatBubbleOutlineIcon fontSize="small" sx={{ mr: 1, fontFamily: 'Cabin' }} />
-                        <Typography variant="body2" color="textSecondary"  sx={{fontFamily: 'Cabin'}}>
+                        <Typography variant="body2" color="textSecondary" sx={{ fontFamily: 'Cabin' }}>
                             {post.numcomments} comments
                         </Typography>
                     </Box>
@@ -110,7 +130,7 @@ const PostItem = ({ post }) => {
             <PostModal
                 open={openModal}
                 handleClose={handleCloseModal}
-                postid={post.postid}
+                post={post}
             />
         </React.Fragment>
     );
