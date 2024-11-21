aa3fa8526a76f248d102c2a81848bab0959170d6
Will Heeney <willheeney03@tamu.edu>
2024-11-21 13:09:36 -0600

    take out tags from create post since not working yet

--- musibara-api/services/posts.py
+++ musibara-api/services/posts.py
@@ -42,8 +42,8 @@ async def createNewPost(post: MusibaraPostType):
     post_id = cursor.fetchone()[0]
     print('post id created: ', post_id)
     db.commit()
-    did_work = await set_post_tags(post['tags'], post_id)
-    print(did_work)
+    # did_work = await set_post_tags(post['tags'], post_id)
+    # print(did_work)
     return {"msg": "success"}
 
 async def getPost(postId: int):
