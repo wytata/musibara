41bf7f00513abf31c330948a849d98fd12a7d59a
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-23 17:27:15 -0600

    Testing

--- musibara-client/src/app/content/posts/[postid]/page.js
+++ musibara-client/src/app/content/posts/[postid]/page.js
@@ -2,24 +2,19 @@
 
 import { useParams } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, Typography, List, Container} from '@mui/material';
+import { Card, CardContent, Typography, List, Container } from '@mui/material';
 import PostItem from '@/components/PostItem';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const CommentLikes = () => {
-  const {postId } = useParams(); 
+    const { postId } = useParams();
 
     const [userPost, setUserPost] = useState(null);
     const fetchUserPosts = async () => {
         const postResponse = await fetch(apiUrl + `/api/content/posts/${postId}`, {
-        credentials: 'include',
+            credentials: 'include',
         });
-
-        console.log(postResponse);
-        if(postResponse.status == 401) {
-        window.location = "/login";
-        }
 
         const jsonData = await postResponse.json()
         setUserPost(jsonData)
@@ -29,15 +24,16 @@ const CommentLikes = () => {
         if (postId) {
             fetchUserPosts(); // Fetch posts when postId is available
         }
+        console.log(postId)
     }, [postId]);
 
-  return (
+    return (
 
-    <Container className="commentPage" sx={{backgroundColor: '#264653', minHeight: '100%', margin: 0, padding: 0 }}>
-        <PostItem key={userPost.postid} post={userPost} />
-    </Container>
+        <Container className="commentPage" sx={{ backgroundColor: '#264653', minHeight: '100%', margin: 0, padding: 0 }}>
+            <PostItem key={userPost.postid} post={userPost} />
+        </Container>
 
-  );
+    );
 }
 
 export default CommentLikes;
