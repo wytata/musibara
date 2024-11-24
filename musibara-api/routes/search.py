5f37c7ddcc28b71930b8883576ab5ca83c368e0d
Wyatt Cade <cadewya@tamu.edu>
2024-11-23 18:16:17 -0600

    Provide minimal search capabilities for users, herds, playlists and posttags

--- musibara-api/routes/search.py
+++ musibara-api/routes/search.py
@@ -7,17 +7,17 @@ from services.search import search_playlists, search_tags, search_herds, search_
 searchRouter = APIRouter()
 
 @searchRouter.post("/playlists")
-async def search_playlists_reponse(request: Request, search_term: Annotated[str, Form()]):
-    return await search_playlists(request, search_term)
+async def search_playlists_reponse(search_term: Annotated[str, Form()]):
+    return await search_playlists(search_term)
 
 @searchRouter.post("/users")
-async def search_users_reponse(request: Request, search_term: Annotated[str, Form()]):
-    return await search_users(request, search_term)
+async def search_users_reponse(search_term: Annotated[str, Form()]):
+    return await search_users(search_term)
 
 @searchRouter.post("/herds")
-async def search_herds_reponse(request: Request, search_term: Annotated[str, Form()]):
-    return await search_herds(request, search_term)
+async def search_herds_reponse(search_term: Annotated[str, Form()]):
+    return await search_herds(search_term)
 
 @searchRouter.post("/tags")
-async def search_tags_reponse(request: Request, search_term: Annotated[str, Form()]):
-    return await search_tags(request, search_term)
+async def search_tags_reponse(search_term: Annotated[str, Form()]):
+    return await search_tags(search_term)
--- musibara-api/services/search.py
+++ musibara-api/services/search.py
@@ -9,8 +9,8 @@ from .user_auth import get_id_username_from_cookie
 from .s3bucket_images import get_image_url
 from services.users import get_current_user
 
-async def generic_search(search_term: Annotated[str, Form()], table_name: str, match_column: str):
-    search_query = f"SELECT *, LEVENSHTEIN(LOWER({match_column}), LOWER(%s)) FROM {table_name} ORDER BY LEVENSHTEIN(LOWER({match_column}), LOWER(%s)) ASC LIMIT 10"
+async def search_playlists(search_term: Annotated[str, Form()]):
+    search_query = f"SELECT *, LEVENSHTEIN(LOWER(name), LOWER(%s)) FROM playlists ORDER BY LEVENSHTEIN(LOWER(name), LOWER(%s)) ASC LIMIT 10"
     try:
         db = get_db_connection()
         cursor = db.cursor()
@@ -23,18 +23,54 @@ async def generic_search(search_term: Annotated[str, Form()], table_name: str, m
         print(e)
         return False
 
-async def search_playlists(request: Request, search_term: Annotated[str, Form()]):
-    results = await generic_search(search_term, "playlists", "name")
-    return results
+async def search_herds(search_term: Annotated[str, Form()]):
+    search_query = f"SELECT *, LEVENSHTEIN(LOWER(name), LOWER(%s)) FROM herds ORDER BY LEVENSHTEIN(LOWER(name), LOWER(%s)) ASC LIMIT 10"
+    try:
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute(search_query, (search_term, search_term, ))
+        rows = cursor.fetchall()
+        columnNames = [desc[0] for desc in cursor.description]
+        search_result = [dict(zip(columnNames, row)) for row in rows]
+        return search_result
+    except Exception as e:
+        print(e)
+        return False
 
-async def search_herds(request: Request, search_term: Annotated[str, Form()]):
-    results = await generic_search(search_term, "herds", "name")
-    return results
+async def search_users(search_term: Annotated[str, Form()]):
+    search_query = f"SELECT *, LEVENSHTEIN(LOWER(username), LOWER(%s)) FROM users ORDER BY LEVENSHTEIN(LOWER(username), LOWER(%s)) ASC LIMIT 10"
+    try:
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute(search_query, (search_term, search_term, ))
+        rows = cursor.fetchall()
+        columnNames = [desc[0] for desc in cursor.description]
+        search_result = [dict(zip(columnNames, row)) for row in rows]
+        return search_result
+    except Exception as e:
+        print(e)
+        return False
 
-async def search_users(request: Request, search_term: Annotated[str, Form()]):
-    results = await generic_search(search_term, "users", "username")
-    return results
-
-async def search_tags(request: Request, search_term: Annotated[str, Form()]):
-    results = await generic_search(search_term, "posttags", "name")
-    return results
+async def search_tags(search_term: Annotated[str, Form()]):
+    search_query = f"SELECT *, LEVENSHTEIN(LOWER(name), LOWER(%s)) FROM posttags ORDER BY LEVENSHTEIN(LOWER(name), LOWER(%s)) ASC LIMIT 10"
+    try:
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute(search_query, (search_term, search_term, ))
+        rows = cursor.fetchall()
+        columnNames = [desc[0] for desc in cursor.description]
+        search_results = [dict(zip(columnNames, row)) for row in rows]
+        for result in search_results:
+            del(result["postid"])
+            del(result["levenshtein"])
+        tags = []
+        seen = set()
+        for result in search_results:
+            print(seen)
+            if result["mbid"].strip() not in seen:
+                seen.add(result["mbid"].strip())
+                tags.append(result)
+        return tags
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={"msg": "Server failed to satisfy your search request."})
