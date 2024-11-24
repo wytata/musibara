7df7f26f1ce3f8229883e9709dffe305494c8b91
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-11-23 18:51:42 -0600

    Indiviual post

--- musibara-client/src/app/content/posts/[postid]/page.js
+++ musibara-client/src/app/content/posts/[postid]/page.js
@@ -2,38 +2,63 @@
 
 import { useParams } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, Typography, List, Container } from '@mui/material';
+import { Card, CardContent, Typography, List, Container , Box} from '@mui/material';
 import PostItem from '@/components/PostItem';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const CommentLikes = () => {
-    const { postId } = useParams();
+const PostDisplay = () => {
+
+    const { postid } = useParams();
+    console.log(postid)
 
     const [userPost, setUserPost] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
     const fetchUserPosts = async () => {
-        const postResponse = await fetch(apiUrl + `/api/content/posts/${postId}`, {
-            credentials: 'include',
-        });
+        setIsLoading(true)
+        try {
+            const postResponse = await fetch(apiUrl + `/api/content/posts/${postid}`, {
+                credentials: 'include',
+            });
 
-        const jsonData = await postResponse.json()
-        setUserPost(jsonData)
+            const jsonData = await postResponse.json()
+            console.log(jsonData);
+            setUserPost(jsonData);
+            setIsLoading(false);
+        }
+        catch {
+            console.error('Error fetching user post:', error);
+        }
     }
 
     useEffect(() => {
-        if (postId) {
-            fetchUserPosts(); // Fetch posts when postId is available
-        }
-        console.log(postId)
-    }, [postId]);
+        fetchUserPosts(); // Fetch posts when postId is available
+        console.log(postid)
+    }, [postid]);
 
+    if (isLoading) {
+        return <p>Loading...</p>;  // Show loading indicator if still fetching
+    }
     return (
 
         <Container className="commentPage" sx={{ backgroundColor: '#264653', minHeight: '100%', margin: 0, padding: 0 }}>
-            <PostItem key={userPost.postid} post={userPost} />
+            <Box sx={{borderRadius: '1rem', color: '#264653', margin: '8px', padding: '10px', width: '100%'}}>
+                <div className="PostContainer" style={{width: '100%'}}>
+                <h1 className='followingTitle' style = {{color: 'white' }}>new posts</h1>
+                
+                        {userPost ? (
+                            <PostItem key={userPost.postid} post={userPost} />
+                        ) : (
+                            <p>No post found for this ID.</p>
+                        )}
+                
+                </div>
+            </Box>
         </Container>
+
+        
 
     );
 }
 
-export default CommentLikes;
+export default PostDisplay;
