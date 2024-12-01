f13f56c702ea1f081e84355e9330a7c07d33fb34
Michael Morris <MRMorris64@tamu.edu>
2024-12-01 14:16:53 -0600

    Last try for dynamic

--- musibara-client/src/components/LinkSpotify.js
+++ musibara-client/src/components/LinkSpotify.js
@@ -27,7 +27,7 @@ export default function LinkSpotifyButton({ username }) {
   
 
     return (
-      <button onClick={linkSpotify} type="button" class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center w-1/3 dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">
+      <button onClick={() => linkSpotify(username)} type="button" class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center w-1/3 dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">
         <Image className='mr-5' src='https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg' width={30} height={30}/>
           Connect Spotify Account
       </button>
