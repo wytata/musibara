e30442d0c059239b0bfad6b29d8496dbe2592100
Wyatt Cade <cadewya@tamu.edu>
2024-12-03 18:48:49 -0600

    Finalize import results

--- validation/playlist-compatibility/import/musicbrainz_import.py
+++ validation/playlist-compatibility/import/musicbrainz_import.py
@@ -7,12 +7,23 @@ musicbrainzngs.set_useragent(
     "https://placeholder.com"
 )
 
-def resolve_isrcs(filename):
+def resolve_isrcs(filename, output_name):
     file = open(filename, "r+")
-    for line in file:
-        isrc = line.strip("\n") 
-    file.close()
+    output_file = open(output_name, "a+")
+    file.write("isrc, num-recordings")
 
-resolve_isrcs("spotify/spotify_popular_songs.txt")
-#resolve_isrcs("apple-music/apple_music_popular.txt")
+    for line in file:
+        isrc = line.strip("\n")
+        try:
+            recording_list = musicbrainzngs.get_recordings_by_isrc(isrc)['isrc']['recording-list']
+            num_results = len(recording_list)
+            print(f"{isrc}, {num_results}", file=output_file)
+        except Exception as e:
+            print(f"{isrc}, 0", file=output_file)
+
+    file.close()
+    output_file.close()
+
+resolve_isrcs("spotify/spotify_popular_songs.txt", "spotify-import-results.csv")
+resolve_isrcs("apple-music/apple_music_popular.txt", "apple-music-import-results.csv")
 
